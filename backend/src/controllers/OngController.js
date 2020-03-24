9427e6f71577b92beb04b0481c215b556c5b3071
Daniel Mello <dmmello1989@gmail.com>
2020-03-24 20:51:46 -0300

    controller wip

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,3 +1,19 @@
+const connection = require("../database/connection");
+//1.3.11
 module.exports = {
-  async create //1.2.40
+  async create(req, res) {
+    const { name, email, whatsapp, city, uf} = req.body;
+    const id = crypto.randomBytes(4).toString("HEX");
+
+    await connection("ongs").insert({ 
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    })
+    
+    return res.json({ id });
+  }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,19 +11,7 @@ routes.get("/ongs", async (req, res) => {
 });
 
 routes.post("/ongs", async (req, res) => {
-  const { name, email, whatsapp, city, uf} = req.body;
-  const id = crypto.randomBytes(4).toString("HEX");
-
-  await connection("ongs").insert({ 
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf
-  })
   
-  return res.json({ id });
 });
 
 module.exports = routes;
