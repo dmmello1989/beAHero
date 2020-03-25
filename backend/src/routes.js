0358b0b7dd8e2811bf932ca36ae8c832c0a8f5dc
Daniel Mello <dmmello1989@gmail.com>
2020-03-25 16:40:52 -0300

    sessions and profile controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,21 @@
 const express = require("express");
-const crypto = require("crypto");
-const connection = require("./database/connection");
+
+const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
-routes.get("/ongs", async (req, res) => {
-  const ongs = await connection("ongs").select("*");
+routes.post("/sessions", SessionController.create);
 
-  return res.json(ongs)
-});
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
 
-routes.post("/ongs", async (req, res) => {
-  
-});
+routes.get("/profile", ProfileController.index);
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.delete);
 
 module.exports = routes;
