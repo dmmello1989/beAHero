b9306f91e4ff1cc16067218ce5d527d64f115ae2
Daniel Mello <dmmello1989@gmail.com>
2020-03-28 21:27:26 -0300

    mobile complete

--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -9,8 +9,8 @@ export default StyleSheet.create({
   },
   header: {
     flexDirection: "row",
-    // justifyContent: "space-between",
-    // alignItems: "center",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   headerText: {
     fontSize: 15,
@@ -25,5 +25,41 @@ export default StyleSheet.create({
     fontSize: 30,
     fontWeight: "bold",
     color: "#13131a"
+  },
+  description: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#737380"
+  },
+  incidentList: {
+    marginTop: 32
+  },
+  incident: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: "#FFFFFF",
+    marginBottom: 16,
+  },
+  incidentProperty: {
+    fontSize: 14,
+    color: "#41414d",
+    fontWeight: "bold"
+  },
+  incidentValue: {
+    marginTop: 8,
+    fontSize: 15,
+    marginBottom: 24,
+    color: "#737380"
+  },
+  detailsButton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  detailsButtonText: {
+    color: "#e02041",
+    fontSize: 15,
+    fontWeight: "bold"
   }
+
 })
